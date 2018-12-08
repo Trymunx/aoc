fc1b0d5ba9bceb5999ca41c94cef8d69c00982b0
Trymunx <trymunx@gmail.com>
2018-12-08 13:24:24 +0000

    More of day 9

--- 09.js
+++ 09.js
@@ -1,3 +1,4 @@
+const example0 = `9 players; last marble is worth 25 points`; // 32
 const example1 = `10 players; last marble is worth 1618 points`; // 8317
 const example2 = `13 players; last marble is worth 7999 points`; // 146373
 const example3 = `17 players; last marble is worth 1104 points`; // 2764
@@ -5,9 +6,31 @@ const example4 = `21 players; last marble is worth 6111 points`; // 54718
 const example5 = `30 players; last marble is worth 5807 points`; // 37305
 
 function answer(input) {
-  let players = input.replace(/^(\d+).*$/, "$1");
+  let numPlayers = input.replace(/^(\d+).*$/, "$1");
   let marbles = input.replace(/^.*worth (\d+) points$/, "$1");
-  console.log(players, marbles);
+  // console.log(players, marbles);
+
+  let circle = [0];
+  let player = 0;
+  let scores = {};
+  let currentIndex = 0;
+  let nextIndex; 
+  let value = 0;
+
+  while (value++ < marbles) {
+    player = (player % numPlayers) + 1;
+    if (value % 23 === 0) {
+      scores[player] += value;
+      scores[player] += circle.splice(currentIndex - 7, 1)[0];
+      // do something with 23
+      nextIndex = false;
+    } else {
+      nextIndex = (currentIndex + 1) % circle.length + 1;
+      circle.splice(nextIndex, 0, value);
+    }
+    console.log(player, circle);
+    // console.log(player, value);
+  }
 }
 
-answer(example1);
+answer(example0);
