1a5e5d2f3b6c974fd0b846eccc1a0e29b2ac8258
Trymunx <trymunx@gmail.com>
2019-12-09 21:50:29 +0000

    linting

--- 2019/08.js
+++ 2019/08.js
@@ -1,13 +1,12 @@
-const input = require('fs').readFileSync('08.txt', 'utf8');
-const example1 = "123456789012";
-const example2 = "0222112222120000";
+const input = require("fs").readFileSync("08.txt", "utf8");
+// const example1 = "123456789012";
+// const example2 = "0222112222120000";
 
 const count = (digit, array) => array.reduce((c, el) => el === digit ? c + 1 : c, 0);
 
 const part1 = (input, width, height) => {
   const inputString = input.trim();
   const layerSize = width * height;
-  const nLayers = inputString.length / layerSize;
 
   const layers = [];
   for (let i = 0; i < inputString.length; i += layerSize) {
@@ -15,8 +14,8 @@ const part1 = (input, width, height) => {
   }
 
   const min = {
-    count: Infinity,
     chksum: 0,
+    count: Infinity,
   };
   layers.forEach(layer => {
     const numZeros = count(0, layer);
@@ -29,14 +28,13 @@ const part1 = (input, width, height) => {
   });
 
   console.log("Part 1:", min.chksum);
-}
+};
 
 const getTransIdx = a => a.reduce((a, el, i) => el === 2 ? [...a, i] : a, []);
 
 const part2 = (input, width, height) => {
   const inputString = input.trim();
   const layerSize = width * height;
-  const nLayers = inputString.length / layerSize;
 
   const layers = [];
   for (let i = 0; i < inputString.length; i += layerSize) {
@@ -58,10 +56,10 @@ const part2 = (input, width, height) => {
   for (let i = 0; i < layerSize; i += width) {
     console.log(output.slice(i, i + width).join(""));
   }
-}
+};
 
 
 // part1(example1, 3, 2);
-part1(input, 25, 6)
+part1(input, 25, 6);
 // part2(example2, 2, 2);
 part2(input, 25, 6);
