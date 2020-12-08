fbb68602181349fc256bf414231bd4d673e39fe3
Trymunx <trymunx@gmail.com>
2020-12-08 13:58:11 +0000

    Mock console.log for imports

--- 2020/index.js
+++ 2020/index.js
@@ -1,3 +1,7 @@
+// Fake console.log so we don't log anything in the imports
+const realLogger = console.log;
+console.log = () => {};
+
 const { day01 } = require("./01.js");
 const { day02 } = require("./02.js");
 const { day03 } = require("./03.js");
@@ -6,6 +10,9 @@ const { day05 } = require("./05.js");
 const { day06 } = require("./06.js");
 const { day07 } = require("./07.js");
 const { day08 } = require("./08.js");
+
+// Reset back to the real console.log
+console.log = realLogger;
 
 console.table([
   ["Part 1", "Part 2"],
